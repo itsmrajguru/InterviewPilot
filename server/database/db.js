b39e821b48e2bd071ba463747b9b7903de3d989a
Mangesh Rajguru <msrajguru7@gmail.com>
2026-05-28 02:01:49 +0530

    updated Database

--- server/database/db.js
+++ server/database/db.js
@@ -1,1 +1,16 @@
-const
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+//creating a database
+async function connectToDB(){
+    try {
+        //connect to blank datasheet provided by this url
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("MongoDB connected Successfully");
+    } catch (e) {
+        console.log("Database Error:", e.message);
+        process.exit(1);  //always keep it as 1
+    }
+}
+
+module.exports={connectToDB}
